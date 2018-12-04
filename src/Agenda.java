e28c6ab4a3de8b1ab85c9de0835cb9344a709fa4
Iosu <ilekubideel18dw@ikzubirimanteo.com>
2018-12-04 21:51:19 +0100

    4.0

--- src/Agenda.java
+++ src/Agenda.java
@@ -7,25 +7,48 @@ public class Agenda {
 		
 		ArrayList<Person> people = new ArrayList<Person>();
 		ArrayList<String> telephones = new ArrayList<String>();
-		ArrayList<String> addresses = new ArrayList<String>();
+		ArrayList<String> adresses = new ArrayList<String>();
 		
 		Scanner sc = new Scanner(System.in);
 		
-		public String viewData() {
+		int index = 0;
+		
+		public int theIndex(String name) {
 			
+			for (int x = 0; x < people.size(); x++) {
+				
+				if (people.get(i).getName().matches(name)) {
+					
+					index = x;
+					
+					break;
+				
+				}
 			
+			}
+
+			return index;
+		}
+		
+		public void addData(Person human, String telephone, String adress) {
+			
+			people.add(human);
+			telephones.add(telephone);
+			adresses.add(adress);
 			
 		}
 		
-		public String addData() {
+		public String viewData(String name) {
 			
-			
+			return people.get(theIndex(name)).toString() + " Phone number: " + telephones.get(theIndex(name)) + " Adress: " + adresses.get(theIndex(name));
 			
 		}
 		
-		public String deleteData() {
+		public String deleteData(String name) {
 			
-			
+			people.remove(theIndex(name));
+			telephones.remove(theIndex(name));
+			adresses.remove(theIndex(name));
 			
 		}
 		
